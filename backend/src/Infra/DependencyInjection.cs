14cc28cbccd11c708dd9b4621037ffb36f04e6e2
Gustavo-Pinaculo <gustavo@pinaculodigital.com.br>
2026-04-24 09:57:16 -0300

    feature/infra

---
+++ backend/src/Infra/DependencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Interfaces;
+using Infra.Data;
+using Infra.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infra;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfra(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<AppDbContext>(opt =>
+            opt.UseNpgsql(config.GetConnectionString("Default")));
+
+        services.AddScoped<ILeadRepository, LeadRepository>();
+        services.AddScoped<ITaskRepository, TaskRepository>();
+        return services;
+    }
+}
---
+++ backend/src/Infra/Repositories/TaskRepository.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Infra.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Repositories;
+
+public class TaskRepository(AppDbContext db) : ITaskRepository
+{
+    public Task<IEnumerable<TaskItem>> GetByLeadIdAsync(int leadId)
+        => db.Tasks.Where(x => x.LeadId == leadId).AsNoTracking()
+               .OrderBy(x => x.CreatedAt).ToListAsync()
+               .ContinueWith(t => t.Result.AsEnumerable());
+
+    public Task<TaskItem?> GetByIdAsync(int leadId, int taskId)
+        => db.Tasks.FirstOrDefaultAsync(x => x.LeadId == leadId && x.Id == taskId);
+
+    public async Task<TaskItem> AddAsync(TaskItem task) { db.Tasks.Add(task); return task; }
+    public void Update(TaskItem task) => db.Tasks.Update(task);
+    public void Remove(TaskItem task) => task.SoftDelete();
+    public Task<int> SaveChangesAsync() => db.SaveChangesAsync();
+}
