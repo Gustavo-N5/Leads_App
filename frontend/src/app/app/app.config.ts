d57bbe356c491a779de8c1cff397f96877e789a0
Gustavo-Pinaculo <gustavo@pinaculodigital.com.br>
2026-04-25 13:07:10 -0300

    adicionando bootstrap

--- frontend/src/app/app/app.config.ts
+++ frontend/src/app/app/app.config.ts
@@ -12,6 +12,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor, errorInterceptor])),
+    provideAnimations(),
     provideAnimations()
-  ]
+]
 };
